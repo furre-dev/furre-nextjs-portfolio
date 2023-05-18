d7d879560eb15e1ac08b3eeb7a14bd93df18a310
Furkan Abay <furkan_abay@hotmail.com>
2023-05-18 16:04:27 +0200

    feat: latest changes

--- src/components/chatGptBot/RecievedMsg.tsx
+++ src/components/chatGptBot/RecievedMsg.tsx
@@ -1,7 +1,7 @@
 export default function RecievedMsg({ text }: { text: string }) {
 	return (
 		<div
-			className="bg-gray-200 mr-auto mt-4 w-max max-w-[80%] h-max px-3 rounded-xl
+			className="bg-gray-200 mr-auto mt-4 w-max max-w-[80%] h-max px-3 rounded-tr-xl rounded-br-xl rounded-bl-xl
         shadow-[0_3px_10px_rgb(0,0,0,0.2)] "
 		>
 			<h1 className=" text-gray-800">{text}</h1>
--- src/components/chatGptBot/SentMsg.tsx
+++ src/components/chatGptBot/SentMsg.tsx
@@ -1,8 +1,8 @@
 export default function SentMsg({ text }: { text: string }) {
 	return (
 		<div
-			className="bg-blue-500 ml-auto w-max max-w-[80%] px-3 py-1 mt-1 rounded-xl my-1
-        shadow-[0_3px_10px_rgb(0,0,0,0.2)]"
+			className="bg-blue-500 ml-auto w-max max-w-[80%] px-3 py-1 mt-1 my-1
+        shadow-[0_3px_10px_rgb(0,0,0,0.2)] rounded-tl-xl rounded-br-xl rounded-bl-xl"
 		>
 			<h1 className="text-right text-white">{text}</h1>
 		</div>
